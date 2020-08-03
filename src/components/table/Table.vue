622905233ce875174af406a76965d9ce714edfdd
Gdx64 <gadu.machado@gmail.com>
2020-08-02 22:22:05 -0300

    initial commit

---
+++ src/components/table/Table.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="table-content">
+    <div class="cards-stack">
+      <Card class="cards-stack__card" v-for="card in cardsList" v-bind:key="card" :cardName="card" />
+    </div>
+    <Hand @cardPlayed="putOnTable($event)" />
+  </div>
+</template>
+
+
+<script lang="ts">
+import Hand from "../hand/Hand.vue";
+import Card from "../hand/Card.vue";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {
+    Hand,
+    Card,
+  },
+})
+export default class Table extends Vue {
+  cardsList = ["cAD"];
+  putOnTable(cardName: string) {
+    this.cardsList.push(cardName);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.cards-stack {
+  display: flex;
+}
+</style>
